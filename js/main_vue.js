c8eeaa2262b5d46fc793720b9886a844afc1a317
CodeAlga <58303506+CodeAlga@users.noreply.github.com>
2020-01-08 10:19:01 +0100

    💤💤💤💤💤💤

--- js/main_vue.js
+++ js/main_vue.js
@@ -11,26 +11,38 @@ let table = new Vue({
     log: function() {
       console.log(this.$data);
     },
-    getData() {
+    /* getData() {
       fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
         headers: {
           "X-API-Key": "E7gpCDZxoNHoNjc5XZE65kKGSRLMIUBHrFQUK9Hi"
         }
       })
-        .then(function(response) {
+        .then((response) => {
           if (response.ok) {
             return response.json();
           }
         })
-        .then(function(data) {
+        .then((data) => {
           table.members = data.results[0].members;
         })
-        .catch(function(error) {
+        .catch((error) => {
           console.log("Request failed: " + error.message);
         });
+    } */
+    getData: function() {
+      table.members = senateData.results[0].members;
     }
-  },
-  created() {
-    this.getData();
   }
+  /* created() {
+    this.getData();
+  } */
 });
+
+let navigation = new Vue({
+  el: "#navbarCollapse",
+  data: {},
+
+  methods: {}
+});
+
+table.getData();
