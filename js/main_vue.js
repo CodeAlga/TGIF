0c7f3729a45115442e767f5486eb832c3b0fe7fd
CodeAlga <58303506+CodeAlga@users.noreply.github.com>
2020-01-09 12:25:02 +0100

    🐝🐝🐝 Updated Vue Js - Table displayed and AllStates array for filter added 🐝🐝🐝

--- js/main_vue.js
+++ js/main_vue.js
@@ -1,16 +1,29 @@
 //
 // VUE JS FOR TABLE
 //
+/* 
+Vue.component("dynamicTable", {
+  data: function() {
+    return {
+      people: []
+    };
+    console.log(people);
+  },
+
+  template:
+    '<tbody><tr v-for="member of members"><td><a v-bind:href="member.url">{{ member.first_name }} {{ member.middle_name }} {{ member.last_name }}</a></td><td>{{ member.party }}</td><td>{{ member.state }}</td><td>{{ member.seniority }}</td><td>{{ member.votes_with_party_pct }}</td></tr></tbody>'
+});
+ */
 
 let table = new Vue({
   el: "#listTbody",
   data: {
     members: []
   },
+
+  template:
+    '<tbody><tr v-for="member of members"><td><a v-bind:href="member.url">{{ member.first_name }} {{ member.middle_name }} {{ member.last_name }}</a></td><td>{{ member.party }}</td><td>{{ member.state }}</td><td>{{ member.seniority }}</td><td>{{ member.votes_with_party_pct }}</td></tr></tbody>',
   methods: {
-    log: function() {
-      console.log(this.$data);
-    },
     /* getData() {
       fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
         headers: {
@@ -31,6 +44,26 @@ let table = new Vue({
     } */
     getData: function() {
       table.members = senateData.results[0].members;
+    },
+
+    states: function(array) {
+      let allStates = ["All States"];
+      filterState = document.getElementById("statesMenu");
+      let newOption = document.createElement("option");
+      newOption.className = "dropdown-item";
+      newOption.value = allStates[0];
+      filterState.append(newOption);
+      newOption.innerHTML = allStates[0];
+      array.forEach((element) => {
+        if (allStates.includes(element.state) === false) {
+          allStates.push(element.state);
+          let newOption = document.createElement("option");
+          newOption.className = "dropdown-item";
+          newOption.value = element.state;
+          filterState.append(newOption);
+          newOption.innerHTML = element.state;
+        }
+      });
     }
   }
   /* created() {
@@ -38,11 +71,5 @@ let table = new Vue({
   } */
 });
 
-let navigation = new Vue({
-  el: "#navbarCollapse",
-  data: {},
-
-  methods: {}
-});
-
 table.getData();
+table.states(table.members);
